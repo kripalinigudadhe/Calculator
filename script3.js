9d1f41e13b4cb5ee29a7fd34b379e275323d3475
kripalinigudadhe <kripalinigudadhe3@gmail.com>
2025-01-03 15:23:17 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,23 @@
+// Function to append clicked number or operator to the display
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+// Function to calculate the result
+function calculateResult() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+function backspace() {
+    const currentDisplay = document.getElementById('display').value;
+    document.getElementById('display').value = currentDisplay.slice(0, -1);
+}
